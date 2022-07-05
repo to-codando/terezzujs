3658e620a7119ef6ee2ac9ed06f77a096777faae
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2022-07-04 21:30:56 -0300

    fix style generator error

--- src/viewFactory.js
+++ src/viewFactory.js
@@ -1,75 +1,91 @@
-import { html, css } from './tagged.js'
-import { uuid } from './uuid.js'
+import { html, css } from "./tagged.js"
+import { uuid } from "./uuid.js"
 
-export const viewFactory = ({ template, events, selector, style = () => {} }) => {
-  let _element = null
-  let _children = {}
+export const viewFactory = ({
+	template,
+	events,
+	selector,
+	style = () => {},
+}) => {
+	let _element = null
+	let _children = {}
 
-  const _initEvents = ({ events, methods, query }) => {
-    for (const key in events) {
-      events[key]({ methods, query })
-    }
-  }
+	const _initEvents = ({ events, methods, query }) => {
+		for (const key in events) {
+			events[key]({ methods, query })
+		}
+	}
 
-  const applyContext = (ctx, text, id) => text.replace(/ctx-/gi, `${ctx}-${id}-`)
+	const applyContext = (ctx, text, id) =>
+		text.replace(/ctx-/gi, `${ctx}-${id}-`)
 
-  const hasStyle = (ctx) => {
-    const head = document.querySelector('head')
-    return head.querySelector(`style[id=${ctx}]`)
-  }
-  const _bindStyles = ({ ctx, id, state }) => {
-    const cssString = style({ css, state })
-    if (cssString && !hasStyle(ctx)) {
-      const styleElement = document.createElement('style')
+	const hasStyle = (ctx, selector) => {
+		const head = document.querySelector("head")
+		return head.querySelector(selector)
+	}
+  
+	const _bindStyles = ({ ctx, id, state }) => {
+		const cssString = style({ css, state })
+		const styleSelector = `style[id=${ctx}]`
 
-      styleElement.setAttribute('id', id)
-      styleElement.textContent = applyContext(ctx, cssString, id)
-      document.head.insertAdjacentElement('beforeend', styleElement)
-    }
-  }
+		if (!cssString) return
 
-  const render = ({ state, methods, target, createChild = () => {} }) => {
-    const query = _element.querySelector.bind(_element)
-    const data = state.get() || {}
-    const ctx = _element.getAttribute('mvc')
-    const id = uuid(ctx)
-    const htmlString = template({ html, state: data, methods })
-    _element.innerHTML = applyContext(ctx, htmlString, id)
-    _initEvents({ element: _element, events, methods, query })
-    _bindStyles({ ctx, id, state: data })
-    renderChildren(_element, createChild)
-  }
+		if (hasStyle(ctx, styleSelector)) {
+      const styleElement = document.querySelector(styleSelector)
+      styleElement.setAttribute('id', ctx)
+			styleElement.textContent = applyContext(ctx, cssString, id)
+      return
+		}
 
-  const renderChildren = (parentElement, create) => {
-    const elements = getElements(parentElement)
-    elements.forEach(element => {
-      const childId = element.getAttribute('mvc')
-      const childModule = _children[childId]
-      const child = create(childModule)
-      child.init(element)
-      // controller.init()
-    })
-  }
+    const styleElement = document.createElement('style')
+    styleElement.setAttribute('id', ctx)
+    styleElement.textContent = applyContext(ctx, cssString, id)
+    document.head.insertAdjacentElement('beforeend', styleElement)
+	}
 
-  const getElements = (parentElement) => {
-    const keys = Object.keys(_children)
-    return keys.flatMap(key => {
-      const selector = `[mvc=${key}]`
-      return Array.from(parentElement.querySelectorAll(selector))
-    })
-  }
 
-  const bindElement = (target) => {
-    _element = target || document.querySelector(selector)
-  }
+	const render = ({ state, methods, target, createChild = () => {} }) => {
+		const query = _element.querySelector.bind(_element)
+		const data = state.get() || {}
+		const ctx = _element.getAttribute("mvc")
+		const id = uuid(ctx)
+		const htmlString = template({ html, state: data, methods })
+		_element.innerHTML = applyContext(ctx, htmlString, id)
+		_initEvents({ element: _element, events, methods, query })
+		_bindStyles({ ctx, id, state: data })
+		renderChildren(_element, createChild)
+	}
 
-  const setChildren = ({ children = {} }) => {
-    _children = children
-  }
+	const renderChildren = (parentElement, create) => {
+		const elements = getElements(parentElement)
+		elements.forEach((element) => {
+			const childId = element.getAttribute("mvc")
+			const childModule = _children[childId]
+			const child = create(childModule)
+			child.init(element)
+			// controller.init()
+		})
+	}
 
-  return {
-    render,
-    bindElement,
-    setChildren
-  }
+	const getElements = (parentElement) => {
+		const keys = Object.keys(_children)
+		return keys.flatMap((key) => {
+			const selector = `[mvc=${key}]`
+			return Array.from(parentElement.querySelectorAll(selector))
+		})
+	}
+
+	const bindElement = (target) => {
+		_element = target || document.querySelector(selector)
+	}
+
+	const setChildren = ({ children = {} }) => {
+		_children = children
+	}
+
+	return {
+		render,
+		bindElement,
+		setChildren,
+	}
 }
